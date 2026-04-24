2cd1d87f070003c8d66e9d28d04e2e142fab4873
vmalychkovych <viktor.malychkovych.knm.2020@gmail.com>
2026-04-24 14:09:59 +0300

    move infrastructure registration to service collection extension

---
+++ TaskFlow.WebAPI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.Application.Interfaces;
+using TaskFlow.Application.Services;
+using TaskFlow.Infrastructure.Persistence;
+using TaskFlow.Infrastructure.Repositories;
+
+namespace TaskFlow.WebAPI.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(
+           this IServiceCollection services,
+           IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ITaskService, TaskService>();
+
+            return services;
+        }
+    }
+}
--- TaskFlow.WebAPI/Program.cs
+++ TaskFlow.WebAPI/Program.cs
@@ -1,31 +1,21 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
-using Microsoft.EntityFrameworkCore;
-using TaskFlow.Application.Interfaces;
-using TaskFlow.Application.Services;
 using TaskFlow.Application.Validators;
-using TaskFlow.Infrastructure.Persistence;
-using TaskFlow.Infrastructure.Repositories;
+using TaskFlow.WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql("Host=localhost;Port=5433;Database=taskflow_db;Username=postgres;Password=postgres"));
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddScoped<ITaskService, TaskService>();
+builder.Services.AddApplicationServices(builder.Configuration);
 
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddValidatorsFromAssemblyContaining<CreateTaskDtoValidator>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
