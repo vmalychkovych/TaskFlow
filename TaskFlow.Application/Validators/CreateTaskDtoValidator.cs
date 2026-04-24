0db788540a6e9e72d58618fc20f947707c0d2ec3
vmalychkovych <viktor.malychkovych.knm.2020@gmail.com>
2026-04-24 12:52:56 +0300

    add fluent validation for create task request

---
+++ TaskFlow.Application/Validators/CreateTaskDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using TaskFlow.Application.DTOs;
+
+namespace TaskFlow.Application.Validators
+{
+    public class CreateTaskDtoValidator : AbstractValidator<CreateTaskDto>
+    {
+        public CreateTaskDtoValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MinimumLength(3).WithMessage("Title must be at least 3 characters.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MinimumLength(5).WithMessage("Description must be at least 5 characters.");
+
+            RuleFor(x => x.ProjectId)
+                .NotEmpty().WithMessage("ProjectId is required.");
+        }
+    }
+}
--- TaskFlow.WebAPI/Program.cs
+++ TaskFlow.WebAPI/Program.cs
@@ -1,6 +1,9 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using TaskFlow.Application.Interfaces;
 using TaskFlow.Application.Services;
+using TaskFlow.Application.Validators;
 using TaskFlow.Infrastructure.Persistence;
 using TaskFlow.Infrastructure.Repositories;
 
@@ -11,6 +14,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql("Host=localhost;Port=5433;Database=taskflow_db;Username=postgres;Password=postgres"));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<ITaskService, TaskService>();
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateTaskDtoValidator>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
