da965c5a063b749e140184efe36afe102784ad80
vmalychkovych <viktor.malychkovych.knm.2020@gmail.com>
2026-04-25 16:12:28 +0300

    refactor task service to use automapper

---
+++ TaskFlow.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,29 @@
+﻿
+using AutoMapper;
+using TaskFlow.Application.DTOs;
+using TaskFlow.Domain.Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TaskFlow.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Workspace, WorkspaceDto>();
+            CreateMap<CreateWorkspaceDto, Workspace>();
+            CreateMap<UpdateWorkspaceDto, Workspace>();
+
+            CreateMap<Project, ProjectDto>();
+            CreateMap<CreateProjectDto, Project>();
+            CreateMap<UpdateProjectDto, Project>();
+
+            CreateMap<TaskItem, TaskDto>()
+                .ForMember(dest => dest.Priority, opt => opt.MapFrom(src => src.Priority.ToString()))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
+
+            CreateMap<CreateTaskDto, TaskItem>();
+            CreateMap<UpdateTaskDto, TaskItem>();
+        }
+    }
+}
